e636c4309c520f121ff426162f78d41556fd1054
unknown <rachael.kate.stokes@gmail.com>
2023-11-10 19:19:02 -0500

    added variables and finished the showscreen functions

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
-var StartScreen = document
+//declaring variables
+var startScreen = document.querySelector('.start');
+var quizScreen = document.querySelector('.quiz');
+var endScreen = document.querySelector('.end');
+var startButton = document.querySelector('#start-btn');
 
 
 
-
+//functions for what is shown on the screen
 function showStart (){
+    startScreen.style.display = null;
+    quizScreen.style.display = "none";
+    endScreen.style.display = "none";
 
 }
 
 function showQuiz(){
+    startScreen.style.display = "none";
+    quizScreen.style.display = null;
+    endScreen.style.display = "none";
 
 }
 
 function showEnd(){
-    StartScreen.style.Sheets.display = "none";
+    startScreen.style.display = "none";
     quizScreen.style.display = "none";
     endScreen.style.display = null;
 
