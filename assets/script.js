39cda4a42d6c63a34d449681020b2549e090ab9a
unknown <rachael.kate.stokes@gmail.com>
2023-11-14 18:34:40 -0500

    stuck on why my questions are now displaying or possible answers displaying on the buttons, followed the class tutorials. will attend office hours again tomorrow

--- assets/script.js
+++ assets/script.js
@@ -5,77 +5,45 @@
 //at the end it will ask for user inititals for saving scores
 
 
-//Quiz questions
-
-var questions = [
-    {
-    number: 1,
-    question: "What is a function in JavaScript?",
-    answer: "A block of code that executes a task when called upon.",
-    options: [
-        "A fun party.",
-        "A block of code that executes a task when called upon.", 
-        "A true or false variable.",
-        "Values made up of letters, numbers, or symbols."
-        ]
-    },
-    {
-    number: 2,
-    question: "A Boolean is a true or false variable.",
-    answer: "True.",
-    options: [
-        "True.",
-        "False."
-        ]  
-    },
-    {
-    number: 3,
-    question: "What is an event listener?",
-    answer: "A function in HTML that waits for an event (such as a click) and reacts to it.",
-    options: [
-        "A function in JavaScript that waits for an event (such as a click) and reacts to it.",
-        "Someone who eavesdrops on parties.",
-        "A function in HTML that waits for an event (such as a click) and reacts to it.",
-        "A base class within which all objects inherit specific properties."
-
-    ]   
-    },
-    {
-    number: 4,
-    question: "An array is a container that houses different data types.",
-    answer: "True.",
-    options: [
-        "True.",
-        "False."
-        ]
-    },
-    {
-    number: 5,
-    question: "In JavaScript, what does this '//' mean?",
-    answer: "Used at the beginning of a comment.",
-    options: [
-        "Used at the end of a comment.",
-        "Or.",
-        "Used at the beginning of a comment.",
-        "And."
-        ]
-    }
-
-]
-
-
-
-
 var startScreen = document.querySelector(".start");
 var quizScreen = document.querySelector(".quiz");
 var endScreen = document.querySelector(".end");
 var startBtn = document.querySelector("#startBtn");
 var questionCard = document.querySelector("questionCard");
 var index = 0;
-var possibleAns = ['1.', '2.', '3.', '4.'];
+//quiz questions
+var questions = [
+    {
+    text:"In JavaScript, what does this '//' mean?",
+    answer: '4. Used at the beginning of a comment',
+    possible:['1. Used at the end of a comment','2. Or.','3. And.', '4. Used at the beginning of a comment'],
+    },
+    {
+    text: "What is an Array?",
+    answer: '1. A container that houses different data types.',
+    possible: ['1. A container that houses different data types.', '2. A string of characters.', '3. A set of numbers only.', '4. A container that houses special characters.'],
+    },
+    {
+    text:"What is an Event Listener?",
+    answer: '3. A function in JavaScript that waits for an event (such as a click) and reacts to it.',
+    possible: ['1. Someone who eavesdrops on parties.', '2. A function in HTML that waits for an event (such as a click) and reacts to it.', 
+               '3. A function in JavaScript that waits for an event (such as a click) and reacts to it.', '4. A base class within which all objects inherit specific properties.'],
+    },
+    {
+    text: "What is a Boolean?",
+    answer: '2. A true/false variable.',
+    possible: [ '1. A string of characters.', '2. A true/false variable.', '3. A function that can be used to create buttons.', '4. A value made up of letters, numbers, and symbols.'],
+    },
+    {
+    text: "What is a function in JavaScript?",
+    answer: '3. A block of code that executes a task when called upon.',
+    possible: [ '1. A true/false variable.', '2. A fun party.', '3. A block of code that executes a task when called upon.', '4. A container that houses different data types.']
+    },
+];
+    
+    
 
-
-
+//show one type of screen at a time for a dynamic experience
 function showStart() {
     startScreen.style.display = null;
     quizScreen.style.display = "none";
@@ -86,7 +54,7 @@ function showQuiz() {
     startScreen.style.display = "none";
     quizScreen.style.display = null;
     endScreen.style.display = "none";
-
+//make sure questions are displayed on the correct screen
     displayQuestion();
 }
 function showEnd() {
@@ -94,21 +62,22 @@ function showEnd() {
     quizScreen.style.display = "none";
     endScreen.style.display = null;
 }
-
+//define displayQuestion function
 function displayQuestion() {
     var possible = questions[index].possible;
     var pEl = document.createElement('p');
     pEl.textContent = questions[index].text;
 
-    questionCard.innerHTML = null;
-    questionCard.appendChild(pEl);
+ 
 
-    for (var i =0; i < possible.length; i++) {
+    for (var i = 0; i < possible.length; i++) {
         var buttonEl = document.createElement('button');
         buttonEl.textContent = possible[i];
-        questionCard.appendChild(buttonEl);
+       
     }
-}
+};
+
+
 startBtn.addEventListener('click', function(event) {
     showQuiz();
 } );
